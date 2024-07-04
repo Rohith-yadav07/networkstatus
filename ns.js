c59de014cb6e1cdf00a494faab0a42531e28e990
Rohith-yadav07 <rohithnaniyadav1122@gmail.com>
2024-07-04 13:34:54 +0530

    networkstatus using js

---
+++ ns.js
@@ -0,0 +1,19 @@
+function myFunction() {
+    if (navigator.onLine) {
+        document.querySelector('span').innerHTML = "You are online now";
+        document.querySelector('p').innerHTML = "Internet is connected";
+        document.querySelector('.icon').classList.remove('offline');
+        document.querySelector('.Network').classList.remove('offline');
+        document.querySelector('.icon i').className = 'uil uil-wifi';
+    } else {
+        document.querySelector('span').innerHTML = 'You are offline now';
+        document.querySelector('p').innerHTML = "Internet is Disconnected";
+        document.querySelector('.icon').classList.add('offline');
+        document.querySelector('.Network').classList.add('offline');
+        document.querySelector('.icon i').className = 'uil uil-wifi-slash'; 
+    }
+}
+
+setInterval(() => {
+    myFunction();
+}, 300);
